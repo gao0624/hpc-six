3115a560d1645e61853752276c9b22f555963fad
gao <13839287160@163.com>
2023-10-28 18:23:13 +0800

    fix

--- text.c
+++ text.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <string.h>
 
@@ -10,13 +11,17 @@ clock_t start_t, end_t;
 int a1 = 1, a2 = 2, a3 = 3;
 int a = 4, b = 5;
 int x;
-int temp = ( 1 && 2 && 3 );
+int ifZero,score,ranNum;
+char msg[] = { 'D','D','D','D','D','D','D','C','B','A' };
 
 void counTime(FunctionPointer func);
+void initValue();
 void judgeOpt_before();
 void judgeOpt_after();
 void optOrder_before();
 void optOrder_after();
+void rmBranch_before();
+void rmBranch_after();
 void start();
 
 int main()
@@ -28,11 +33,31 @@ int main()
     return 0;
 }
 
+void initValue(){
+    /**
+     * creat random num 1-100 srand: prevent same num
+     * 
+     * srand(time(NULL));这一行代码会影响
+    */
+    srand(time(NULL));
+    ranNum = rand() % 100 + 1;
+    printf("%d\n",ranNum);
+
+    /**
+     * if && zero
+    */
+    ifZero = ( a1 && a2 && a3 );
+
+    /**
+     * ranNum -> score
+    */
+    score = ranNum;
+}
+
 void counTime(FunctionPointer func){
 
     start_t = clock();
-    for (long i = 0; i < 1000000000; i++) {
-        // 这里可以放您的代码
+    for (long i = 0; i < 2000000000; i++) {
         func();
     }
     end_t = clock();
@@ -50,24 +75,57 @@ void judgeOpt_before(){
 
 void judgeOpt_after(){
     
-    if( temp ){
+    if( ifZero ){
         int sum = a + b;
     }
 }
 
 void optOrder_before(){
-    
-    if(a > 0)
-        x = a;
-    else
-        x = b;
+
+    for(int i = 1; i < 4; i++){
+        if(a > 0){
+            x = a;
+        }else
+            x = b;
+    }
 }
 
 void optOrder_after(){
-    x = (a > 0 ? a : b);
+    for(int i = 1; i < 4; i++){
+        x = (a > 0 ? a : b);
+    }
+}
+
+void rmBranch_before(){
+
+    for(int i = 1; i < 5; i++){
+        if( score >= 90 ){
+            //printf("A")
+        }
+        else if( score >= 80 ){
+            //printf("B")
+        }
+        else if( score >= 70 ){
+            //printf("C")
+        }
+        else{
+            //printf("D")
+        }
+    }
+}
+
+void rmBranch_after(){
+    for(int i = 1; i < 5; i++){
+        msg[score/10];
+    }
+    //printf("%c",msg[score/10])
 }
 
 void start(){
+    /**
+     * initValue
+    */
+    initValue();
 
     counTime(judgeOpt_before);
     printf("-----------------------------------------------------------------------------\n");
@@ -78,4 +136,9 @@ void start(){
     counTime(optOrder_before);
     printf("-----------------------------------------------------------------------------\n");
     counTime(optOrder_after);
+
+    printf("\n");
+    counTime(rmBranch_before);
+    printf("-----------------------------------------------------------------------------\n");
+    counTime(rmBranch_after);
 }
